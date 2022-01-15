653c7203de96805404d8ffbb60e3d49a17585781
Nadia Jamhari <62807626+nadiajamhari@users.noreply.github.com>
2022-01-15 15:37:08 +0800

    EmployeeManagement with  MainApp (Test)

--- src/com/epms/bean/EmployeeManagement.java
+++ src/com/epms/bean/EmployeeManagement.java
@@ -1,11 +1,15 @@
 package com.epms.bean;
 
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 
@@ -14,8 +18,11 @@ import java.util.Calendar;
 import java.text.DateFormat;  
 import java.text.SimpleDateFormat;  
 import java.util.Date;
+import java.util.Iterator;
+import java.util.ListIterator;
 
 public class EmployeeManagement{
+	private static Scanner x;
 	   
     ArrayList<Attendance> arrayOfAttendance= new ArrayList<>();
     ArrayList<Attendance> arrayOfMarkAttendance = new ArrayList<>();
@@ -25,8 +32,10 @@ public class EmployeeManagement{
 	FileInputStream attendanceFile = null;
 	
 	Scanner input = new Scanner(System.in);
+	Scanner input2= new Scanner(System.in);
 	
-	public void init() {
+	//read Data of Employee
+	public void initReadEmployeeFile() {
 		try {
 			    employeeFile = new FileInputStream("employee.txt");     
 				Scanner sc=new Scanner(employeeFile); 
@@ -43,7 +52,8 @@ public class EmployeeManagement{
 		}  
 	}
 	
-	public void init2() {
+	//read Data of Attendance
+	public void initReadAttendanceFile() {
 		try {
 		    attendanceFile = new FileInputStream("attendance.txt");     
 			Scanner sc=new Scanner(attendanceFile); 
@@ -52,13 +62,11 @@ public class EmployeeManagement{
 				if ( line.trim().length() == 0 ) {
 				    continue;  // Skip blank lines
 				}
-				
-				String parts1 = line.replace(":",",");
-				String[] words=parts1.split(",");
+				String[] words=line.split(",");
 				boolean isPresent=Boolean.parseBoolean(words[2]);
 				arrayOfAttendance.add(new Attendance(words[0],Integer.parseInt(words[1]),isPresent));
 			
-			} 
+			}
 			sc.close();     //closes the scanner  
 		}  
 		catch(IOException e)  {  
@@ -66,20 +74,213 @@ public class EmployeeManagement{
 		} 
 	}
 	
-	public void displayEmployee(){
-		for(Employee str: arrayOfEmployee){
-			System.out.println("employeeId: "+str.getEmployeeId()+", name: " + str.getName() +", gender: " +str.getGender());
+	//add New Employee
+	public boolean addEmployee(int id, String name, char gender) {
+		writeEmployee(id,name,gender);
+		return arrayOfEmployee.add(new Employee(id, name, gender));
+	}
+	
+	//add New Employee at "employee.txt"
+	public void writeEmployee(int id, String name, char gender) {
+			try {
+				
+				BufferedWriter bw = new BufferedWriter(new FileWriter(new File(
+						"employee.txt"), true));
+				String text =id+","+name+"," +gender;
+				bw.write(text);
+				bw.newLine();
+				bw.close();
+			    System.out.println("Attedance Succesfully");
+			    } catch (IOException e) {
+			      System.out.println("An error occurred.");
+			      e.printStackTrace();
+			   }
+	}
+		
+	//Update Employee
+	public boolean setEmployee() {
+		String name; String gender;
+		System.out.print("Enter Employee  Id to update: ");
+		int id = input.nextInt();
+		System.out.println("----------------------------");
+		
+		if(getEmployee(id) != null) {
+			System.out.print("Enter name: ");
+			name = input2.nextLine();
+			System.out.print("Enter gender(M/F): ");
+			gender = input2.nextLine();
 			
+			ListIterator<Employee> i = arrayOfEmployee.listIterator();
+			while(i.hasNext()) {
+				Employee e = i.next();
+				if(e.getEmployeeId() == id) {
+					char g = gender.charAt(0);
+					i.set(new Employee(id, name, g));
+					editRecordEmployee("employee.txt",id,name,g);
+					return true;
+				}
+				
+			}
+		}else {
+			System.out.println("Record not found");
+			
+		}
+		return false;
+	}
+	
+	//update employee Data in "employee.txt"
+	public void editRecordEmployee(String filepath, int IDtoEdit, String newName, char newGender){
+		String tempFile = "temp.txt";
+		File oldFile = new File(filepath);
+		File newFile = new File(tempFile);
+		
+		
+		int ID;
+		String name = "";
+		char gender;
+		
+		try {
+			
+			FileWriter fw = new FileWriter(tempFile, true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			PrintWriter pw = new PrintWriter(bw);
+			x = new Scanner (new File(filepath));
+			x.useDelimiter("[,\n]");
+			
+			while(x.hasNext()) {
+				ID = Integer.parseInt(x.next());
+				name = x.next();
+				gender = x.next().charAt(0);
+				
+				if(ID == IDtoEdit) {
+					pw.print(ID+ ","+newName+ ","+newGender+"\n");
+				}else {
+					pw.print(ID+ ","+name+ ","+gender+"\n");
+				}
+				
+			}
+			
+			x.close();
+			pw.flush();
+			pw.close();
+			
+			
+			oldFile.delete();
+			File dump = new File(filepath);
+			newFile.renameTo(dump);
+		
+		    System.out.println("Edit Succesfully");
+		    } catch (IOException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		   }
+	}
+	
+	//get Employee Details
+	public Employee getEmployee(int id) {
+		for(Employee e : arrayOfEmployee ) {
+			if(e.getEmployeeId() == id) {
+				return e;
+			}
+		}
+		
+		return null;
+	}
+	
+	//deleteEmployee
+	public String deleteEmployee() {
+		System.out.print("Enter the Employee ID : ");
+		int id = input.nextInt();
+		Iterator<Employee> i = arrayOfEmployee.iterator();
+		while(i.hasNext()) {
+			Employee e = i.next();
+			if(e.getEmployeeId() == id) {
+				deleteRecordEmployee(id);
+				arrayOfEmployee.remove(e);
+				return "Sucessfully Remove "+id+" from Employee Data";
+			}
+			
+		}
+		return "Unable to remove "+id+" as it not exist in the Employee Data";
+	}
+	
+	//delete data Employee from "employee.txt"
+	public void deleteRecordEmployee(int employeeId) {
+		try {
+			File inputFile = new File("employee.txt");
+			File tempFile = new File(inputFile.getAbsolutePath() + ".tmp");
+         
+			BufferedReader br = new BufferedReader(new FileReader("employee.txt"));
+            PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
+            String line = null;
+            
+            //Read from the original file and write to the new
+            //unless content matches data to be removed.
+            while ((line = br.readLine()) != null) {
+            	if ( line.trim().length() == 0 ) {
+				    continue;  // Skip blank lines
+				}
+            	String[] words = line.trim().split(",");
+                if (Integer.parseInt(words[0]) != employeeId) {
+                    pw.println(line);
+                    pw.flush();
+                }
+            }
+            pw.close();
+            br.close();
+        	inputFile.delete();
+			File dump = new File("employee.txt");
+			tempFile.renameTo(dump);
+		
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	//display the list of Employee
+	public void displayListOfEmployee(){
+		if(arrayOfEmployee.isEmpty()) {
+			System.out.println("No Employee List");
+		}
+		else {
+			System.out.println("\n------------------");
+			System.out.println(" Employee List");
+			System.out.println("--------------------\n");
+			System.out
+					.println("----------------------------------------------------------------");
+			System.out.println("Employee ID\t Name\t Gender");
+			System.out
+					.println("----------------------------------------------------------------\n");
+			for(Employee str: arrayOfEmployee){
+				System.out.printf("%1$s\t\t\t%2$s\t\t%3$s\n",
+						str.getEmployeeId(), str.getName(), str.getGender());
+			}	
 	   }
 	}
 	
-	public void markAttendance() {
+	// mark Attendance for All
+	public String markAttendance() {
 		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy"); 
 		Date date = new Date();
 		String strDate = formatter.format(date);
         System.out.println("Mark Attendance for "+strDate);
         
-		System.out.println("Please State 1 for Present and 2 for Absent ");
+        if(checkExistDate(strDate)==true) {
+        	System.out.println("You already mark the Attendance Today. You want to re-mark the Attendance?\n"
+        			+ "Choose: 1. Re-mark, 2. No :");
+        	int remark = input.nextInt();
+        	if(remark == 1) {
+        		deleteExistingRecordAttendance(strDate);
+        	}
+        	
+        	else {
+        		return "No re-mark the attendance";
+        	}
+        }
+
+		System.out.println("Please State 1. Present, 2. Absent for Each Employee");
 		//loop the Employee Array
 		for(Employee str: arrayOfEmployee){
 			System.out.print("employeeId: "+str.getEmployeeId()+", name: " + str.getName() +", gender: " +str.getGender()+ " : ");
@@ -89,8 +290,59 @@ public class EmployeeManagement{
 	   }
 		
 	   writeAttendance();
+	   return "Succesfully Mark The Attendance !";
 	}
+	
+	public void deleteExistingRecordAttendance(String date) {
+		arrayOfAttendance.clear();
+		try {
+			File inputFile = new File("attendance.txt");
+			File tempFile = new File(inputFile.getAbsolutePath() + ".tmp");
+         
+			BufferedReader br = new BufferedReader(new FileReader("attendance.txt"));
+            PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
+            String line = null;
+            
+            //Read from the original file and write to the new
+            //unless content matches data to be removed.
+            while ((line = br.readLine()) != null) {
+            	if ( line.trim().length() == 0 ) {
+				    continue;  // Skip blank lines
+				}
+            	String[] words = line.trim().split(",");
+                if (!words[0].equals(date)) {
+                    pw.println(line);
+                    pw.flush();
+                    boolean isPresent=Boolean.parseBoolean(words[2]);
+    				arrayOfAttendance.add(new Attendance(words[0],Integer.parseInt(words[1]),isPresent));
+                }
+            }
+            pw.close();
+            br.close();
+        	inputFile.delete();
+			File dump = new File("attendance.txt");
+			tempFile.renameTo(dump);
 		
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+			
+	
+		
+	}
+	
+    //Check in "attendance.txt" the date has been mark the attendance or not
+	public boolean checkExistDate(String date) {
+		for(Attendance str: arrayOfAttendance) {
+			if(str.getDate().equalsIgnoreCase(date)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	// add the attendance listt in "attendance.txt"
+	
 	public void writeAttendance() {
 		try {
 			
@@ -98,7 +350,7 @@ public class EmployeeManagement{
 					"attendance.txt"), true));
 			for(Attendance str: arrayOfMarkAttendance){
 				arrayOfAttendance.add(new Attendance(str.getDate(),str.getEmployeeId(),str.isPresent));
-				String text =str.getDate()+":"+str.getEmployeeId()+"," + str.isPresent;
+				String text =str.getDate()+","+str.getEmployeeId()+"," + str.isPresent;
 			      bw.write(text);
 			      bw.newLine();
 			}
@@ -111,37 +363,45 @@ public class EmployeeManagement{
 		   }
 	}
 
+	//get all attendance list
+	
 	public void getAttendanceList() {
-		System.out.println("Attendance List");
-		System.out.println("\n------------------");
-		System.out.println(" Attendance List");
-		System.out.println("--------------------\n");
-		System.out
-				.println("----------------------------------------------------------------");
-		System.out.println("Date \t\tEmployee ID\tis Present Status");
-		System.out
-				.println("----------------------------------------------------------------\n");
-		for (Attendance str: arrayOfAttendance) {
-			String currentDate = str.getDate();
-			System.out.printf("%1$s\t%2$s\t\t%3$s\n",
-					str.getDate(), str.getEmployeeId(), str.isPresent);
+		if(arrayOfAttendance.isEmpty()) {
+			System.out.println("No Attendance List");
 		}
-//		for(Attendance str: arrayOfAttendance){
-//			String text ="Date" +str.getDate()+": EmployeeId"+str.getEmployeeId()+"," + str.isPresent;
-//		}
+		else {
+			System.out.println("\n------------------");
+			System.out.println(" Attendance List");
+			System.out.println("--------------------\n");
+			System.out
+					.println("----------------------------------------------------------------");
+			System.out.println("Date \t\tEmployee ID\tis Present Status");
+			System.out
+					.println("----------------------------------------------------------------\n");
+			for (Attendance str: arrayOfAttendance) {
+				String currentDate = str.getDate();
+				System.out.printf("%1$s\t%2$s\t\t%3$s\n",
+						str.getDate(), str.getEmployeeId(), str.isPresent);
+			}
+		}
+		
 	}
 
+	//get list of attendance for the specific employee
+	
 	public void getAttendance(int employeeId) {
+		String text="";
 		System.out.println("An Attendance for "+employeeId);
 		for(Attendance str: arrayOfAttendance){
 			if(str.getEmployeeId() == employeeId) {
-				String text =str.getDate()+":"+str.getEmployeeId()+"," + str.isPresent;
+				text =str.getDate()+":"+str.getEmployeeId()+"," + str.isPresent;
 				System.out.println(text);
 			}
 		}
+		
+		if(text.equals("")) {
+			System.out.println("Record Attendance for Employee ID "+employeeId+" not found");
+		}
 	}
 	
-	public void checkDate(String date) {
-		
-	}
 }
--- src/com/epms/bean/MainApp.java
+++ src/com/epms/bean/MainApp.java
@@ -7,26 +7,47 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.XML");
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.XML");
 		EmployeeManagement obj = (EmployeeManagement) context.getBean("employeeManagement");//read id
 
-		obj.init();
-		//store attendance in arrayList
-		obj.init2();
-		obj.displayEmployee();
+	
 		
-		//attendance();
-		System.out.println();
-		obj.markAttendance();
-		System.out.println();
-		obj.getAttendanceList();
-		System.out.println();
+		obj.initReadEmployeeFile();
+		obj.initReadAttendanceFile();
+		
+//		//displayEmployee
+//		obj.displayListOfEmployee();
+		
+		//editEmployee
+		System.out.println("Before Update");
+		obj.displayListOfEmployee();
+		obj.setEmployee();
+		System.out.println("After Update");		
+		obj.displayListOfEmployee();
 		
 		
-		//getSpecific Attendance
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter Employee ID : ");
-	    int Id = input.nextInt();
-	    obj.getAttendance(Id);
+//		//delete Employee Testing
+//		System.out.println("Before Delete");
+//		obj.displayListOfEmployee();
+//		System.out.println(obj.deleteEmployee());
+//		System.out.println("After Delete");
+//		obj.displayListOfEmployee();
+		
+	
+//		obj.getAttendanceList();
+		
+//		//attendance();
+//		System.out.println();
+//		obj.markAttendance();
+//		System.out.println();
+//		obj.getAttendanceList();
+//		System.out.println();
+//		
+//		
+//		//getSpecific Attendance
+//		Scanner input = new Scanner(System.in);
+//		System.out.print("Enter Employee ID : ");
+//	    int Id = input.nextInt();
+//	    obj.getAttendance(Id);
 	}
 }
